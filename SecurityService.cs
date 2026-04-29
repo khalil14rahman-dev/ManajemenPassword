be820172e4a60c79bb2bb5a556961289242e6182
L4vende12 <naufalghufran2006@gmail.com>
2026-04-29 11:25:37 +0700

    Update SecurityService.cs

--- SecurityService.cs
+++ SecurityService.cs
@@ -5,35 +5,25 @@ namespace Project_KPL_ManajemenPassword
 {
     public static class SecurityService
     {
-        /// <summary>
-        /// Fungsi untuk mengenkripsi (mengacak) teks agar tidak bisa dibaca langsung di file JSON.
-        /// </summary>
-        /// <param name="plainText">Password asli yang diinput user</param>
-        /// <returns>Teks yang sudah diacak dalam format Base64</returns>
+        /// <param name="plainText"
+ 
         public static string Encrypt(string plainText)
         {
             if (string.IsNullOrEmpty(plainText)) return "";
 
-            // Mengubah string menjadi byte array
             var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
 
-            // Mengubah byte array menjadi string Base64 (Enkripsi sederhana)
             return Convert.ToBase64String(plainTextBytes);
         }
 
-        /// <summary>
-        /// Fungsi untuk mendekripsi (mengembalikan) teks yang diacak menjadi password asli.
-        /// </summary>
-        /// <param name="base64EncodedData">Teks acak dari file JSON</param>
-        /// <returns>Password asli yang bisa dibaca user</returns>
+
+        /// <param name="base64EncodedData">
         public static string Decrypt(string base64EncodedData)
         {
             if (string.IsNullOrEmpty(base64EncodedData)) return "";
 
-            // Mengubah string Base64 kembali menjadi byte array
             var base64EncodedBytes = Convert.FromBase64String(base64EncodedData);
 
-            // Mengubah byte array kembali menjadi string teks biasa
             return Encoding.UTF8.GetString(base64EncodedBytes);
         }
     }
